5ef89435d18ceb868e7d6c16b881d54fe3699362
Talha <ranatalhaa01@gmail.com>
2022-02-03 01:23:34 +0500

    Header HTML CSS COMPLETED

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6XYwepIWZL0IEwHcrBWQW8mPipj2Avxo",
+  authDomain: "facebook-clone-a983f.firebaseapp.com",
+  projectId: "facebook-clone-a983f",
+  storageBucket: "facebook-clone-a983f.appspot.com",
+  messagingSenderId: "521960309096",
+  appId: "1:521960309096:web:d6fbf40b8061bd67ab16dd",
+  measurementId: "G-QE0L72F5DD",
+};
