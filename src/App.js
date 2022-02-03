6164bb3ec6e174120dcceff49b2474a45d5b6833
Talha <ranatalhaa01@gmail.com>
2022-02-03 18:55:40 +0500

    working on the feed section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import Feed from "./Feed";
 
 function App() {
   return (
     <div className='app'>
       <Header />
-      <Sidebar />
+
+      <div className='app__body'>
+        <Sidebar />
+        <Feed />
+      </div>
     </div>
   );
 }
---
+++ src/StoryReel.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./StoryReel.css";
+import Story from "./Story";
+
+function StoryReel() {
+  return (
+    <div className='storyReel'>
+      <Story
+        className='storyReel__story'
+        image='https://www.thecorrespondent.pk/wp-content/uploads/2021/10/muhammad-ali.jpeg'
+        profileSrc='https://www.essentiallysports.com/stories/olympics-news-muhammad-ali-and-other-athletes-that-were-nominated-for-grammy-awards/assets/1.jpeg'
+        title='Ali'
+      />
+      <Story
+        className='storyReel__story'
+        image='https://www.thecorrespondent.pk/wp-content/uploads/2021/10/muhammad-ali.jpeg'
+        profileSrc='https://www.essentiallysports.com/stories/olympics-news-muhammad-ali-and-other-athletes-that-were-nominated-for-grammy-awards/assets/1.jpeg'
+        title='Ali'
+      />
+      <Story
+        className='storyReel__story'
+        image='https://www.thecorrespondent.pk/wp-content/uploads/2021/10/muhammad-ali.jpeg'
+        profileSrc='https://www.essentiallysports.com/stories/olympics-news-muhammad-ali-and-other-athletes-that-were-nominated-for-grammy-awards/assets/1.jpeg'
+        title='Ali'
+      />
+      <Story
+        className='storyReel__story'
+        image='https://www.thecorrespondent.pk/wp-content/uploads/2021/10/muhammad-ali.jpeg'
+        profileSrc='https://www.essentiallysports.com/stories/olympics-news-muhammad-ali-and-other-athletes-that-were-nominated-for-grammy-awards/assets/1.jpeg'
+        title='Ali'
+      />
+      <Story
+        className='storyReel__story'
+        image='https://www.thecorrespondent.pk/wp-content/uploads/2021/10/muhammad-ali.jpeg'
+        profileSrc='https://www.essentiallysports.com/stories/olympics-news-muhammad-ali-and-other-athletes-that-were-nominated-for-grammy-awards/assets/1.jpeg'
+        title='Ali'
+      />
+    </div>
+  );
+}
+
+export default StoryReel;
